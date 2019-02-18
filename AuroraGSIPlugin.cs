55fe9fa2d87509ce1d04036ee5b911e6a443c2b5
Will Bennion <will.bennion@outlook.com>
2019-02-18 11:36:05 +0000

    Initial version of the plugin.

---
+++ AuroraGSIPlugin.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using OsuRTDataProvider;
+using Sync.Plugins;
+using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace OsuAuroraMod {
+    
+    [SyncRequirePlugin(typeof(OsuRTDataProviderPlugin))]
+    public class AuroraGSIPlugin : Plugin {
+
+        // Meta-data
+        public const string PLUGIN_NAME = "AuroraGSI";
+        public const string PLUGIN_AUTHOR = "Wibble199";
+        public const string VERSION = "1.0.0";
+
+        // HTTP/GSI variables
+        private HttpClient client = new HttpClient();
+        private RootGSINode gsiNode = new RootGSINode();
+
+        // Required constructor
+        public AuroraGSIPlugin() : base(PLUGIN_NAME, PLUGIN_AUTHOR) { }
+
+        public override void OnEnable() {
+            try {
+                // Get a reference to the OsuRTDataProviderPlugin and it's ListenerManager
+                var dp = (OsuRTDataProviderPlugin)getHoster().EnumPluings().First(plugin => plugin.Name == "OsuRTDataProvider");
+                var lm = dp.ListenerManager;
+
+                // Add events and update the payload
+                lm.OnStatusChanged += (_, status) => { gsiNode.game.status = status.ToString(); SendUpdate(); };
+                lm.OnAccuracyChanged += acc => { gsiNode.game.accuracy = acc; SendUpdate(); };
+                lm.OnHealthPointChanged += hp => { gsiNode.game.hp = hp; SendUpdate(); };
+                lm.OnComboChanged += combo => { gsiNode.game.combo = combo; SendUpdate(); };
+                lm.OnCount50Changed += count => { gsiNode.game.count50 = count; SendUpdate(); };
+                lm.OnCount100Changed += count => { gsiNode.game.count100 = count; SendUpdate(); };
+                lm.OnCount300Changed += count => { gsiNode.game.count300 = count; SendUpdate(); };
+                lm.OnCountKatuChanged += count => { gsiNode.game.countKatu = count; SendUpdate(); };
+                lm.OnCountGekiChanged += count => { gsiNode.game.countGeki = count; SendUpdate(); };
+                lm.OnCountMissChanged += count => { gsiNode.game.countMiss = count; SendUpdate(); };
+
+            } catch { }
+        }
+
+        // Serialize and send the updates to Aurora
+        private void SendUpdate() {
+            string json = JsonConvert.SerializeObject(gsiNode);
+            client.PostAsync("http://localhost:9088/", new StringContent(json));
+        }
+    }
+}
---
+++ GSIPayload.cs
@@ -0,0 +1,27 @@
+﻿namespace OsuAuroraMod {
+
+    internal class RootGSINode {
+        public ProviderNode provider = new ProviderNode();
+        public GameNode game = new GameNode();
+    }
+
+    class GameNode {
+        public string status = "";
+        public double hp = 0;
+        public double accuracy = 0;
+        public int combo = 0;
+
+        public int count50 = 0;
+        public int count100 = 0;
+        public int count200 = 0;
+        public int count300 = 0;
+        public int countKatu = 0;
+        public int countGeki = 0;
+        public int countMiss = 0;
+    }
+
+    class ProviderNode {
+        public string name = "osu";
+        public int appid = -1;
+    }
+}
